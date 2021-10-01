956274caeeb4b8fcd861e25be34a3d1eae4c2861
uidoyen <nasiba.ansari@gmail.com>
2021-10-01 16:04:07 +0530

    initial commit

--- src/app/modules/RoasterManagement/RoasterManagement.tsx
+++ src/app/modules/RoasterManagement/RoasterManagement.tsx
@@ -1,12 +1,67 @@
 import React from 'react';
 import { PageSection, Title } from '@patternfly/react-core';
+import moment from "moment";
+import Timeline from "react-calendar-timeline";
+import generateFakeData from "./utils/randomData";
 
-const RoasterManagement: React.FC = () => (
-  <PageSection>
-    <Title headingLevel="h1" size="lg">
-      Roaster Management
-    </Title>
-  </PageSection>
-);
+var keys = {
+  groupIdKey: "id",
+  groupTitleKey: "title",
+  groupRightTitleKey: "rightTitle",
+  itemIdKey: "id",
+  itemTitleKey: "title",
+  itemDivTitleKey: "title",
+  itemGroupKey: "group",
+  itemTimeStartKey: "start",
+  itemTimeEndKey: "end",
+  groupLabelKey: "title"
+};
+
+const RoasterManagement: React.FC = () => {
+
+  const handleItemMove = (itemId, dragTime, newGroupOrder) => {
+    const { items, groups } = this.state;
+
+    const group = groups[newGroupOrder];
+
+    this.setState({
+      items: items.map(item =>
+        item.id === itemId
+          ? Object.assign({}, item, {
+              start: dragTime,
+              end: dragTime + (item.end - item.start),
+              group: group.id
+            })
+          : item
+      )
+    });
+
+    console.log("Moved", itemId, dragTime, newGroupOrder);
+  };
+
+  const handleItemResize = (itemId, time, edge) => {
+    const { items } = this.state;
+
+    this.setState({
+      items: items.map(item =>
+        item.id === itemId
+          ? Object.assign({}, item, {
+              start: edge === "left" ? time : item.start,
+              end: edge === "left" ? item.end : time
+            })
+          : item
+      )
+    });
+
+    console.log("Resized", itemId, time, edge);
+  };
+  return (
+    <PageSection>
+      <Title headingLevel="h1" size="lg">
+        Roaster Management
+      </Title>
+    </PageSection>
+  )
+}
 
 export default RoasterManagement;
